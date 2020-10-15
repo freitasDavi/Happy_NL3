75fc9ad61b2b1d35c5eabde93e9335f5d15ab6d7
Davi Freitas da Silva <davi.frrs@outlook.com>
2020-10-15 13:32:58 -0300

    Novas páginas adcionadas e Abstração de COmponentes

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,46 +1,61 @@
-import React from 'react';
+import React from "react";
 
-import '../styles/pages/orphanagesMap.css';
-import mapMarkerImg from '../images/Local.svg';
-import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { Link } from "react-router-dom";
+import { FiPlus, FiArrowRight } from "react-icons/fi";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 
-import 'leaflet/dist/leaflet.css';
+import mapMarkerImg from "../images/Local.svg";
+import mapIcon from "../utils/mapIcon";
 
+import "../styles/pages/orphanagesMap.css";
 
-function OrphanagesMap () {
-    return (
-        <div id="page-map">
-            <aside>
-                <header>
-                    <img src={mapMarkerImg} alt="Happy"/>
+function OrphanagesMap() {
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy" />
 
-                    <h2>Escolha um Orfanato no mapa</h2>
-                    <p>Muitas crianças estão esperando a sua visita :)</p>
-                </header>
+          <h2>Escolha um Orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
 
+        <footer>
+          <strong>Içara</strong>
+          <span>Santa Catarina</span>
+        </footer>
+      </aside>
 
-                <footer>
-                    <strong>Içara</strong>
-                    <span>Santa Catarina</span>
-                </footer>
-            </aside>
+      <Map
+        center={[-28.7209272, -49.2942842]}
+        zoom={15}
+        style={{ width: "100%", height: "100%" }}
+      >
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
 
-            <Map
-                center={[-28.7209272,-49.2942842]}
-                zoom={15}
-                style={{ width:"100%", height: "100%"}}
-            > 
-                {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
-                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
-            </Map>
-
-            <Link to="" className="create-orphanage">
-                <FiPlus size={32} color="#fff" />
+        <Marker icon={mapIcon} position={[-28.7209272, -49.2942842]}>
+          <Popup
+            closeButton={false}
+            minWidth={240}
+            maxWidth={240}
+            className="map-popup"
+          >
+            Lar dos Marvados
+            <Link to="/orphanages/1">
+              <FiArrowRight size={20} color={"#fff"} />
             </Link>
-        </div>
-    );
+          </Popup>
+        </Marker>
+      </Map>
+
+      <Link to="/orphanages/create" className="create-orphanage">
+        <FiPlus size={32} color="#fff" />
+      </Link>
+    </div>
+  );
 }
 
 export default OrphanagesMap;
